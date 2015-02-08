b1cc1f15bfc21c3397035a966da968246da985a7
Abraham Epton <abraham.epton@gmail.com>
2015-02-08 12:37:29 -0600

    Fixing another bug in loader script

--- loader.sh
+++ loader.sh
@@ -4,7 +4,7 @@ PROJECT=the_el
 ROOT=/home/ubuntu/$PROJECT
 SECRETS=/home/ubuntu/secrets.sh
 
-export DJANGO_SETTINGS_MODULE='the_el.settings'
+export DJANGO_SETTINGS_MODULE=the_el.settings
 . /home/ubuntu/.virtualenvs/$PROJECT/bin/activate
 
 cd $ROOT
