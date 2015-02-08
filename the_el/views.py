6878083b9a0a6b388c32cbcb556f720bb8cd58a2
Abraham Epton <abraham.epton@gmail.com>
2015-02-08 11:56:13 -0600

    Adding views

---
+++ the_el/views.py
@@ -0,0 +1,34 @@
+from django.db import connection
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+import json
+from datetime import date
+
+from the_el.models import Route, Trip, Stop
+
+
+def index(request):
+    routes = Route.objects.all()
+    return render(request, 'index.html', {
+        'routes': routes
+        })
+
+
+def route(request, pkey):
+    route = Route.objects.get(pk=pkey)
+    trips = Trip.objects.filter(route=route)
+    shapes = set()
+    all_shapes = []
+    trip_ids = set()
+    for trip in trips:
+        if trip.shape.shape_id not in shapes:
+            shapes.add(trip.shape.shape_id)
+            all_shapes.append(trip.shape.as_json())
+            #shape = list(trip.shape.line_string.coords)
+            #shape.append(trip.shape.line_string.coords[0])
+            #all_shapes.append(shape)
+    return render(request, 'route.html', {
+        'route': route,
+        'trips': trips,
+        'shapes': json.dumps(all_shapes),
+        })
