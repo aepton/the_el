180325180d9f03b6ccdff4ff881bab7572b81b03
Abraham Epton <abraham.epton@gmail.com>
2015-02-08 12:26:22 -0600

    Adding loader

---
+++ loader.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+PROJECT=the_el
+ROOT=/home/ubuntu/$PROJECT
+SECRETS=/home/ubuntu/secrets.sh
+
+export DJANGO_SETTINGS_MODULE="the_el.settings"
+source /home/ubuntu/.virtualenvs/$PROJECT/bin/activate
+
+cd $ROOT
+. $SECRETS
+python /home/ubuntu/$PROJECT/manage.py load_positions
